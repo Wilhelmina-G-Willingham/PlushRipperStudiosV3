43511cca134aaa15c8d348a54899a165cb4c82ea
Wilhelmina-G-Willingham <jwm025@student.usc.edu.au>
2024-05-15 17:50:50 +1000

    Implemented Cody's environment, Touched up Tool movement script to be more versatile, Made Cody's tools work with tool systems
    Added two new test scenes for the environment (one contextless, one with tool functions and bear), made the tool movement script base the height off the snap-back location, rather than the world origin. Gave the tools the correct functions to a prototypical standard

--- BCI304_TeddyBearHospitalProject/Assets/Scripts/TopDownCamera/ToolScripts/ToolMovement.cs
+++ BCI304_TeddyBearHospitalProject/Assets/Scripts/TopDownCamera/ToolScripts/ToolMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class ToolMovement : MonoBehaviour, IInteractible
@@ -9,17 +10,21 @@ public class ToolMovement : MonoBehaviour, IInteractible
 
     [SerializeField]
     private float targetHeight = 6f;
+    private float realHeight;
 
     private Vector3 originalPosition;
 
     private void Start()
     {
         originalPosition = transform.position; // Store the original position
+        realHeight = parentLocation.transform.position.y + targetHeight;
     }
 
     // When the object is held, move in relation to the mouse
     public void Interact()
     {
+        
+
         Vector3 objectPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.WorldToScreenPoint(transform.position).z);
         Vector3 worldPos = Camera.main.ScreenToWorldPoint(objectPos);
         transform.position = new Vector3(worldPos.x, targetHeight, worldPos.z);
